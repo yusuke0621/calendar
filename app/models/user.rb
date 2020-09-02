993b515a0b6f95b82de3e5999a2bf8916e622894
yusuke0621 <yusuke19930621@gmail.com>
2020-09-02 19:45:16 +0900

    vslidation change

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :comments
 
-  validates :nickname, :email, presence: true, length: { maximum: 6 }
+  validates :nickname, presence: true, length: { maximum: 6 }
+  validates :email, presence: true
   validates :password, presence: true, format: { with:  /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]{6,100}+\z/ }, on: :create
 end
